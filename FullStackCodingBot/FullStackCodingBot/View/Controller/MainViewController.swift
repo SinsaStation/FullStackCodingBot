1f6a397b4caa8b977c750c139214753e176dc425
Ray suh <readingod@naver.com>
2021-07-05 23:41:17 +0900

    chore: [#14] setupMethod삭제 viewmodel -> bindViewmodel 메서드로 이동

--- FullStackCodingBot/FullStackCodingBot/View/Controller/MainViewController.swift
+++ FullStackCodingBot/FullStackCodingBot/View/Controller/MainViewController.swift
@@ -1,6 +1,4 @@
 import UIKit
-import RxSwift
-import RxCocoa
 
 final class MainViewController: UIViewController, ViewModelBindableType {
    
@@ -9,22 +7,9 @@ final class MainViewController: UIViewController, ViewModelBindableType {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setup()
     }
     
     func bindViewModel() {
-        print("\(self)")
-    }
-    
-}
-
-private extension MainViewController {
-    
-    private func setup() {
-        setupButtonController()
-    }
-    
-    private func setupButtonController() {
         buttonController.setupButton()
         buttonController.bind { [unowned self] viewController in
             self.viewModel.makeMoveAction(to: viewController)
