37551cd14a61d131245a8a5b32bc92cb7f990def
Ray suh <readingod@naver.com>
2021-07-05 12:01:21 +0900

    feat: [#14] 하단 스택뷰 및 버튼 추가

--- FullStackCodingBot/FullStackCodingBot/ViewController.swift
+++ FullStackCodingBot/FullStackCodingBot/ViewController.swift
@@ -1,19 +1,12 @@
-//
-//  ViewController.swift
-//  FullStackCodingBot
-//
-//  Created by Song on 2021/07/05.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.layer.masksToBounds = true
+        view.layer.cornerRadius = 5
     }
-
 
 }
 
