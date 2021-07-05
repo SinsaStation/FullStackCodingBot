9fd5fc769f2e2ef666f492e8978a478103e849f6
Ray suh <readingod@naver.com>
2021-07-05 12:12:59 +0900

    feat: [#14] ButtonController 및 Mapper 객체 생성

--- FullStackCodingBot/FullStackCodingBot/ButtonController.swift
+++ FullStackCodingBot/FullStackCodingBot/ButtonController.swift
@@ -1,5 +1,36 @@
-import Foundation
+import UIKit
+
+enum ViewControllerType: CaseIterable {
+    case gift
+    case ad
+    case rank
+    case item
+}
+
+final class ButtonMapper {
+    private var map: [UIButton: ViewControllerType]
+    
+    init(from buttons: [UIButton]) {
+        self.map = Dictionary(uniqueKeysWithValues: zip(buttons, ViewControllerType.allCases))
+    }
+    
+    subscript(button: UIButton) -> ViewControllerType? {
+        return map[button]
+    }
+}
 
 final class ButtonController: NSObject {
     
+    @IBOutlet var moveToVCButtons: [UIButton]!
+    
+    private var moveToVCMapper: ButtonMapper?
+    private var buttonTouchedHandler: (ViewControllerType) -> ()
+    
+    override init() {
+        buttonTouchedHandler = { _ in }
+    }
+    
+    func setupButton() {
+        self.moveToVCMapper = ButtonMapper(from: moveToVCButtons)
+    }
 }
