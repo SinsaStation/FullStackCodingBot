97307d9bf632b1bd4bbea1eeb153fef4448bb731
Ray suh <readingod@naver.com>
2021-07-06 11:24:44 +0900

    chore: [#19] 불필요한 view 삭제 collectionview로 구현 예정

--- FullStackCodingBot/FullStackCodingBot/View/Controller/MainViewController.swift
+++ FullStackCodingBot/FullStackCodingBot/View/Controller/MainViewController.swift
@@ -5,10 +5,6 @@ final class MainViewController: UIViewController, ViewModelBindableType {
     var viewModel: MainViewModel!
     @IBOutlet var buttonController: ButtonController!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
     func bindViewModel() {
         buttonController.setupButton()
         buttonController.bind { [unowned self] viewController in
--- FullStackCodingBot/FullStackCodingBot/View/CustomView/ItemView.swift
+++
@@ -1,6 +0,0 @@
-import UIKit
-
-class ItemView: XibView {
-
-    
-}
