8f51d1445843d191b75710dc779b1e25b1b9f6a8
Song Lee <songlish233@gmail.com>
2021-07-06 20:35:07 +0900

    chore: [#20] 오토레이아웃 조정

--- FullStackCodingBot/FullStackCodingBot/View/Controller/GameViewController.swift
+++ FullStackCodingBot/FullStackCodingBot/View/Controller/GameViewController.swift
@@ -15,6 +15,10 @@ final class GameViewController: UIViewController, ViewModelBindableType {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         gameStart()
     }
     
