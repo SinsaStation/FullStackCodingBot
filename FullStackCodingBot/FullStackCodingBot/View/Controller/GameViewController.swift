bbfc89a840189686e2f763f6f75276b167cf1d39
Song Lee <songlish233@gmail.com>
2021-07-06 11:35:27 +0900

    feat: [#20] 뷰 코드 스토리보드 이전

--- FullStackCodingBot/FullStackCodingBot/View/Controller/GameViewController.swift
+++ FullStackCodingBot/FullStackCodingBot/View/Controller/GameViewController.swift
@@ -1,13 +1,49 @@
 import UIKit
 
-class GameViewController: UIViewController, ViewModelBindableType {
+final class GameViewController: UIViewController, ViewModelBindableType {
+    
+    @IBOutlet weak var unitPerspectiveView: UnitPerspectiveView!
     
     var viewModel: GameViewModel!
+    
+    private let unitCount = 8
+    private let allUnits = [Unit(image: .swift), Unit(image: .kotlin), Unit(image: .java), Unit(image: .cPlusPlus)]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        gameStart()
     }
     
     func bindViewModel() {
         print("\(self)")
+    }
+    
+    private func gameStart() {
+        let startingUnits = generateStartingUnits(count: unitCount)
+        unitPerspectiveView.configure(with: startingUnits)
+        unitPerspectiveView.fillUnits()
+    }
+    
+    @IBAction func leftButtonTouched(_ sender: UIButton) {
+        buttonAction()
+    }
+    
+    @IBAction func rightButtonTouched(_ sender: UIButton) {
+        buttonAction()
+    }
+    
+    
+    private func buttonAction() {
+        unitPerspectiveView.removeFirstUnit()
+        unitPerspectiveView.refillLastUnit(with: newRandomUnit())
+        unitPerspectiveView.fillUnits()
+    }
+    
+    private func generateStartingUnits(count: Int) -> [Unit] {
+        return (0..<count).map{ allUnits[$0 % 4] }
+    }
+    
+    private func newRandomUnit() -> Unit {
+        return allUnits.randomElement()!
     }
 }
