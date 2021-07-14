78dc68bab23fcc8ca2d14e71ad4c31776b3f5ce5
Song Lee <songlish233@gmail.com>
2021-07-14 15:23:29 +0900

    feat: [#40]  게임오버 시 획득한 돈 저장

--- FullStackCodingBot/FullStackCodingBot/ViewModel/GameOverViewModel.swift
+++ FullStackCodingBot/FullStackCodingBot/ViewModel/GameOverViewModel.swift
@@ -6,12 +6,25 @@ import Action
 class GameOverViewModel: CommonViewModel {
     
     private(set) var finalScore: Int
+    private(set) var moneyGained: Int
+    private(set) lazy var currentMoney: Observable<Int> = {
+        return storage.availableMoeny()
+    }()
     private var newGameStatus: BehaviorRelay<GameStatus>
     
     init(sceneCoordinator: SceneCoordinatorType, storage: ItemStorageType, finalScore: Int, newGameStatus: BehaviorRelay<GameStatus>) {
         self.finalScore = finalScore
+        self.moneyGained = finalScore / 10
         self.newGameStatus = newGameStatus
         super.init(sceneCoordinator: sceneCoordinator, storage: storage)
+    }
+    
+    func execute() {
+        storeReward()
+    }
+    
+    private func storeReward() {
+        storage.raiseMoney(by: self.moneyGained)
     }
     
     func makeMoveAction(to viewController: GameOverViewControllerType) {
