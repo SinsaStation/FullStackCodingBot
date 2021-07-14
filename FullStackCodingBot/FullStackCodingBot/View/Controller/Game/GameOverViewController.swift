48852f29fb453ce6db1c274f3d170c1214fb9adb
Song Lee <songlish233@gmail.com>
2021-07-14 15:23:49 +0900

    feat: [#40] 게임오버 화면에 획득 돈, 현재 돈 표시

--- FullStackCodingBot/FullStackCodingBot/View/Controller/Game/GameOverViewController.swift
+++ FullStackCodingBot/FullStackCodingBot/View/Controller/Game/GameOverViewController.swift
@@ -3,12 +3,15 @@ import UIKit
 final class GameOverViewController: UIViewController, ViewModelBindableType {
     
     @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var gainedCoinLabel: UILabel!
+    @IBOutlet weak var totalCoinLabel: UILabel!
     @IBOutlet var buttonController: GameOverButtonController!
     
     var viewModel: GameOverViewModel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewModel.execute()
     }
     
     func bindViewModel() {
@@ -18,5 +21,10 @@ final class GameOverViewController: UIViewController, ViewModelBindableType {
         }
         
         scoreLabel.text = "\(viewModel.finalScore)"
+        gainedCoinLabel.text = "\(viewModel.moneyGained)"
+        viewModel.currentMoney
+            .subscribe(onNext: { [unowned self] currentMoney in
+                self.totalCoinLabel.text = "\(currentMoney)"
+        }).disposed(by: rx.disposeBag)
     }
 }
