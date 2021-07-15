ca73f6f692d89ebfe6b9a2cf33c0f398d66ef1ee
Ray suh <readingod@naver.com>
2021-07-15 14:18:18 +0900

    feat: [#14] TitleLabel Animation 추가

--- FullStackCodingBot/FullStackCodingBot/View/Controller/Main/MainViewController.swift
+++ FullStackCodingBot/FullStackCodingBot/View/Controller/Main/MainViewController.swift
@@ -1,16 +1,19 @@
 import UIKit
+import GhostTypewriter
 
 final class MainViewController: UIViewController, ViewModelBindableType {
-   
+    
     var viewModel: MainViewModel!
     @IBOutlet var buttonController: MainButtonController!
+    @IBOutlet weak var titleLabel: TypewriterLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        titleLabel.startTypewritingAnimation()
     }
     
     func bindViewModel() {
-       buttonController.setupButton()
+        buttonController.setupButton()
         buttonController.bind { [unowned self] viewController in
             self.viewModel.makeMoveAction(to: viewController)
         }
