b2d8472a18bf9d0a9ae924d2989eee8327c0d420
Song Lee <songlish233@gmail.com>
2021-07-17 13:10:27 +0900

    feat: [#49] 유닛 릴즈의 개수, 크기, 위치 조정

--- FullStackCodingBot/FullStackCodingBot/Constant/SyetemSettings.swift
+++ FullStackCodingBot/FullStackCodingBot/Constant/SyetemSettings.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 enum GameSetting {
-    static let count = 8
+    static let count = 7
     static let maxUnitCount = 8
     static let startingTime: Int64 = 60
     static let wrongTime = 10
