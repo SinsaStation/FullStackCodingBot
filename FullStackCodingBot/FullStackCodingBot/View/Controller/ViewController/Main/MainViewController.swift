35f40395fea72b7796aa2010a26db3577973037d
Song Lee <songlish233@gmail.com>
2021-07-21 19:05:31 +0900

    feat: [#65] 타이틀 애니메이션 폰트 적용
    - Attributed Text 활용

--- FullStackCodingBot/FullStackCodingBot/View/Controller/ViewController/Main/MainViewController.swift
+++ FullStackCodingBot/FullStackCodingBot/View/Controller/ViewController/Main/MainViewController.swift
@@ -25,8 +25,10 @@ final class MainViewController: UIViewController, ViewModelBindableType {
     }
     
     private func setupTitleLabel() {
-        titleLabel.font = .systemFont(ofSize: view.bounds.width * 0.04)
-        titleLabel.text = Text.title
+        let font = UIFont(name: Font.joystix, size: view.bounds.width * 0.04) ?? UIFont()
+        let attributedString = NSMutableAttributedString(string: Text.title)
+        attributedString.addAttribute(.font, value: font, range: .init(location: 0, length: Text.title.count))
+        titleLabel.attributedText = attributedString
     }
     
     func bindViewModel() {
