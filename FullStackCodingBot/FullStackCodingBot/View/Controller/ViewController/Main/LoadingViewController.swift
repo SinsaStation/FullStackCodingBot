64449a37e32fdb4f6638a002568d6cf99ed7df3f
Ray suh <readingod@naver.com>
2021-07-30 13:06:26 +0900

    feat: [#108] LoadingVC 생성

---
+++ FullStackCodingBot/FullStackCodingBot/View/Controller/ViewController/Main/LoadingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoadingViewController.swift
+//  Full Stack
+//
+//  Created by 서우석 on 2021/07/30.
+//
+
+import UIKit
+
+class LoadingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
