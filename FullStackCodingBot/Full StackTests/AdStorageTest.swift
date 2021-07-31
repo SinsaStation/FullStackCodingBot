fd3d5a5371bba1eb1f2900b15347b51d9b536849
Song Lee <songlish233@gmail.com>
2021-07-31 17:22:41 +0900

    test: [#110] 시간에 따른 스토리지 업데이트

--- FullStackCodingBot/Full StackTests/AdStorageTest.swift
+++ FullStackCodingBot/Full StackTests/AdStorageTest.swift
@@ -1,25 +1,25 @@
 import XCTest
 
 class AdStorageTest: XCTestCase {
+    
+    private let now = Date()
+    private var adStorage: AdStorageType!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let emptyInformation = AdsInformation.empty()
+        adStorage = MockAdStorage(with: emptyInformation)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func test_ShouldUpdateRewards() throws {
+        let yesterday = Calendar.current.date(byAdding: .day, value: -1, to: now)!
+        let yesterdayInformation = AdsInformation(ads: [], lastUpdated: yesterday, gift: nil)
+        let isUpdated = adStorage.setNewRewardsIfPossible(with: yesterdayInformation)
+        XCTAssertEqual(isUpdated, true)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func test_ShouldNotUpdateRewards() throws {
+        let currentInformation = AdsInformation(ads: [], lastUpdated: now, gift: nil)
+        let isUpdated = adStorage.setNewRewardsIfPossible(with: currentInformation)
+        XCTAssertEqual(isUpdated, false)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
