5562498a9a1d08452dafd1c7c442c860d1933a13
Ray suh <readingod@naver.com>
2021-08-04 12:07:53 +0900

    chore: 메서드 및 프로토콜 네이밍 변경

--- FullStackCodingBot/FullStackCodingBot/Constant/Extension.swift
+++ FullStackCodingBot/FullStackCodingBot/Constant/Extension.swift
@@ -1,36 +1,36 @@
 import Foundation
 
 
-protocol ObjectSavable {
-    func setStruct<Object>(_ object: Object, forKey: String) throws where Object: Encodable
-    func getStruct<Object>(forKey: String, castTo type: Object.Type) throws -> Object where Object: Decodable
+protocol StructSavable {
+    func setStruct<Object: Encodable>(_ object: Object, forKey: String) throws
+    func getStruct<Object: Decodable>(forKey: String, castTo type: Object.Type) throws -> Object
 }
 
-enum ObjectSavableError: Error {
+enum StructSavableError: Error {
     case unableToDecode
     case unableToEncode
     case noValue
 }
 
-extension UserDefaults: ObjectSavable {
-    func setStruct<Object>(_ object: Object, forKey: String) throws where Object: Encodable {
+extension UserDefaults: StructSavable {
+    func setStruct<Object: Encodable>(_ object: Object, forKey: String) throws {
         let encoder = JSONEncoder()
         do {
             let data = try encoder.encode(object)
             set(data, forKey: forKey)
         } catch {
-            throw ObjectSavableError.unableToEncode
+            throw StructSavableError.unableToEncode
         }
     }
     
-    func getStruct<Object>(forKey: String, castTo type: Object.Type) throws -> Object where Object: Decodable {
-        guard let data = data(forKey: forKey) else { throw ObjectSavableError.noValue }
+    func getStruct<Object: Decodable>(forKey: String, castTo type: Object.Type) throws -> Object {
+        guard let data = data(forKey: forKey) else { throw StructSavableError.noValue }
         let decoder = JSONDecoder()
         do {
             let object = try decoder.decode(type, from: data)
             return object
         } catch {
-            throw ObjectSavableError.unableToDecode
+            throw StructSavableError.unableToDecode
         }
     }
 }
