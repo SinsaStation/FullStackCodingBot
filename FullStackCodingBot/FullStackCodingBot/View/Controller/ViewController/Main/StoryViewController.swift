a033ed4c147058002695b5c2e5a5456286721281
Ray suh <readingod@naver.com>
2021-08-06 14:07:04 +0900

    feat: [#131] storyVC 및 stor scene 생성

---
+++ FullStackCodingBot/FullStackCodingBot/View/Controller/ViewController/Main/StoryViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+final class StoryViewController: UIViewController, ViewModelBindableType {
+    
+    var viewModel: MainViewModel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func bindViewModel() {
+    }
+}
