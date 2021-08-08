5b892f3f6342a1d32293f272633da1ee5bac9f3a
Song Lee <songlish233@gmail.com>
2021-08-08 20:33:18 +0900

    chore: [#131] 스크립트 수정

--- FullStackCodingBot/FullStackCodingBot/Constant/Story/Script.swift
+++ FullStackCodingBot/FullStackCodingBot/Constant/Story/Script.swift
@@ -4,14 +4,14 @@ struct Script {
     
     static let all: [Script] = [
         .init(line: .init(text: "\"Know Yeah Technology\"", special: .intro), animation: .zoom),
-        .init(speaker: .worker, line: .init(text: "프로젝트 마무리 됐습니다", emotion: .normal)),
+        .init(speaker: .worker, line: .init(text: "프로젝트 마무리됐습니다", emotion: .normal)),
         .init(speaker: .ceo, line: .init(text: "(검토...)", emotion: .normal)),
-        .init(speaker: .ceo, line: .init(text: "여기 수정하게.\n여기도, 여기도, 여기도...", emotion: .normal)),
+        .init(speaker: .ceo, line: .init(text: "여기 수정하게.\n여기도, 여기도, 여기도...", emotion: .notGood)),
         .init(speaker: .worker, line: .init(text: "저 그런데...\n프론트 개발자들이 퇴사를 해서", emotion: .normal)),
-        .init(speaker: .worker, line: .init(text: "이 부분은 수정이 불가능 할 것 같은데...", emotion: .notGood)),
-        .init(speaker: .ceo, line: .init(text: "무슨 소리야?\n자네도 개발자 아닌가?", emotion: .normal)),
+        .init(speaker: .worker, line: .init(text: "이 부분은 수정이 불가능할 것 같은데요", emotion: .notGood)),
+        .init(speaker: .ceo, line: .init(text: "무슨 소리야?\n자네도 개발자 아닌가?", emotion: .bad)),
         .init(speaker: .worker, line: .init(text: "(...)", emotion: .bad)),
-        .init(speaker: .ceo, line: .init(text: "뭐하고 있나?\n어서 나가서 시작하게", emotion: .normal)),
+        .init(speaker: .ceo, line: .init(text: "뭐하고 있나?\n어서 나가서 시작하게", emotion: .notGood)),
         .init(line: .init(text: "으아아아아아악!!!", special: .outro), animation: .shake)
     ]
     
