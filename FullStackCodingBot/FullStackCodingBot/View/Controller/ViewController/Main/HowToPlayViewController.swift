1f0f6749da98968170709ce748e0272ad41cee0c
Ray suh <readingod@naver.com>
2021-08-10 17:03:45 +0900

    feat: [#141]좌우 스와이프 통하여 화면이동 구현

--- FullStackCodingBot/FullStackCodingBot/View/Controller/ViewController/Main/HowToPlayViewController.swift
+++ FullStackCodingBot/FullStackCodingBot/View/Controller/ViewController/Main/HowToPlayViewController.swift
@@ -20,11 +20,13 @@ final class HowToPlayViewController: UIViewController, ViewModelBindableType {
     }
 }
 
+// MARK: SetUp
 private extension HowToPlayViewController {
     
     private func setup() {
         setupImagePageControl()
         setupManual(from: 0)
+        setupSwipeGesture()
     }
     
     private func setupImagePageControl() {
@@ -39,5 +41,39 @@ private extension HowToPlayViewController {
         let manual = viewModel.getCurrentManul(from: index)
         howToImageView.image = UIImage(named: manual.image)
         howToTextView.text = manual.text
+    }
+    
+    private func setupSwipeGesture() {
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(respondToSwipeGesture(_:)))
+        swipeLeft.direction = UISwipeGestureRecognizer.Direction.left
+        howToImageView.addGestureRecognizer(swipeLeft)
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(respondToSwipeGesture(_:)))
+        swipeRight.direction = UISwipeGestureRecognizer.Direction.right
+        howToImageView.addGestureRecognizer(swipeRight)
+    }
+}
+
+// MARK: Action
+private extension HowToPlayViewController {
+    
+    @objc private func respondToSwipeGesture(_ gesture: UIGestureRecognizer) {
+        guard let swipeGesture = gesture as? UISwipeGestureRecognizer else {
+            return
+        }
+        
+        switch swipeGesture.direction {
+        case .left:
+            guard imagePageControl.currentPage < 4 else { return }
+            imagePageControl.currentPage += 1
+        
+        case .right:
+            guard imagePageControl.currentPage > 0 else { return }
+            imagePageControl.currentPage -= 1
+        
+        default:
+            break
+        }
+        setupManual(from: imagePageControl.currentPage)
     }
 }
