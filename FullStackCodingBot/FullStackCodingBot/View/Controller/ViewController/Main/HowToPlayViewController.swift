5e34ea868675e71458fec348d43ffd8535309127
Ray suh <readingod@naver.com>
2021-08-10 15:54:26 +0900

    feat: [#141]imageView, pagecontrol, textView 추가

--- FullStackCodingBot/FullStackCodingBot/View/Controller/ViewController/Main/HowToPlayViewController.swift
+++ FullStackCodingBot/FullStackCodingBot/View/Controller/ViewController/Main/HowToPlayViewController.swift
@@ -4,6 +4,10 @@ final class HowToPlayViewController: UIViewController, ViewModelBindableType {
     
     var viewModel: HowToPlayViewModel!
     
+    @IBOutlet weak var howToImageView: UIImageView!
+    @IBOutlet weak var imagePageControl: UIPageControl!
+    @IBOutlet weak var howToTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
