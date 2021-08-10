d458b23c3e0c7a68f66085a95b31a278acefecaa
Ray suh <readingod@naver.com>
2021-08-10 15:25:37 +0900

    feat: [#141]viewcontroller swift 파일 생성

---
+++ FullStackCodingBot/FullStackCodingBot/View/Controller/ViewController/Main/HowToPlayViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+final class HowToPlayViewController: UIViewController, ViewModelBindableType {
+    
+    var viewModel: MainViewModel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func bindViewModel() {
+    }
+}
