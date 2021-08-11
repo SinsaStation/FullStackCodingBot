921fd90fdccad480d11608e2e67afd9c21d7f092
Ray suh <readingod@naver.com>
2021-08-11 12:36:13 +0900

    refacotr: coredata error print삭제 후 throw error

--- FullStackCodingBot/FullStackCodingBot/Service/ErrorManager/ErrorManager.swift
+++ FullStackCodingBot/FullStackCodingBot/Service/ErrorManager/ErrorManager.swift
@@ -4,3 +4,8 @@ enum DataParsingError: Error {
     case cannotTransformToString
     case cannotTransformToStruct
 }
+
+enum CoreDataError: Error {
+    case cannotFetchData
+    case cannotSaveData
+}
