a9d931f57040890687fdd44461695a26bba1ef74
Song Lee <songlish233@gmail.com>
2021-08-17 13:53:33 +0900

    feat: [#159] Banner Ad 로드

--- FullStackCodingBot/FullStackCodingBot/View/Controller/ViewController/Main/MainViewController.swift
+++ FullStackCodingBot/FullStackCodingBot/View/Controller/ViewController/Main/MainViewController.swift
@@ -3,6 +3,7 @@ import RxSwift
 import RxCocoa
 import GameKit
 import Firebase
+import GoogleMobileAds
 
 final class MainViewController: UIViewController, ViewModelBindableType {
     
@@ -10,15 +11,18 @@ final class MainViewController: UIViewController, ViewModelBindableType {
     @IBOutlet var buttonController: MainButtonController!
     @IBOutlet weak var skyView: SkyView!
     @IBOutlet weak var titleView: TypeWriterView!
+    @IBOutlet weak var bannerView: GADBannerView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setTitleViewObserver()
+        setBanner()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         titleView.show(text: Text.title)
+        bannerView.load(GADRequest())
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -38,5 +42,10 @@ final class MainViewController: UIViewController, ViewModelBindableType {
             .subscribe(onNext: { [unowned self ] _ in
                 self.titleView.layoutSubviews(with: Text.title)
             }).disposed(by: rx.disposeBag)
+    }
+    
+    private func setBanner() {
+        bannerView.adUnitID = IdentiferAD.banner
+        bannerView.rootViewController = self
     }
 }
