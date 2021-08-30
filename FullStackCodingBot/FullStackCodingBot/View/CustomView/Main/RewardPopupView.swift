edb31cd09aa8a897eadc434a0c2264308fb348c9
Song Lee <songlish233@gmail.com>
2021-08-30 12:24:14 +0900

    feat: [#185] 리워드 팝업에 컬러 애니메이션 추가

--- FullStackCodingBot/FullStackCodingBot/View/CustomView/Main/RewardPopupView.swift
+++ FullStackCodingBot/FullStackCodingBot/View/CustomView/Main/RewardPopupView.swift
@@ -2,4 +2,31 @@ import UIKit
 
 final class RewardPopupView: UIView {
     
+    @IBOutlet weak var backgroundView: UIView!
+    
+    private lazy var feverColors: [UIColor] = {
+        return UnitInfo.allCases.map { UIImage(named: $0.detail.image)?.averageColor ?? UIColor() }
+    }()
+    
+    func hide() {
+        isHidden = true
+    }
+    
+    func show() {
+        isHidden = false
+        backgroundAnimation(duration: 10, colors: feverColors, repeatCount: .infinity)
+    }
+    
+    private func backgroundAnimation(duration: Double, colors: [UIColor], repeatCount: Float) {
+        let backgroundColorKey = #keyPath(CALayer.backgroundColor)
+        let backgroundColorAnimation = CAKeyframeAnimation(keyPath: backgroundColorKey)
+        backgroundColorAnimation.values = colors.map { $0.cgColor }
+        backgroundColorAnimation.timingFunction = CAMediaTimingFunction(name: .easeOut)
+        backgroundColorAnimation.duration = duration
+        backgroundColorAnimation.calculationMode = .linear
+        backgroundColorAnimation.repeatCount = repeatCount
+        backgroundColorAnimation.isRemovedOnCompletion = false
+        backgroundView.layer.removeAllAnimations()
+        backgroundView.layer.add(backgroundColorAnimation, forKey: backgroundColorKey)
+    }
 }
